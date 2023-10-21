d49ecf1dc3ad6fd28c234d671acaddf67c76337d
blue <evga7@naver.com>
2023-10-21 09:27:53 +0900

    init

---
+++ src/test/java/com/pyo/cutalbum/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyo.cutalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
